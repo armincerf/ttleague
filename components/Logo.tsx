8707b821f0484fff8573adcbd3ca10db683b02cc
Alex Davis <alx@juxt.pro>
2024-10-20 22:16:55 +0100

    link logo to leaderboard

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Logo({ className = "" }: { className?: string }) {
 	return (
 		<div className={`w-full h-24 relative ${className}`}>
-			<Image
-				fill
-				src="/mkttl-logo.png"
-				alt="MK Table Tennis Logo"
-				priority
-				sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-				style={{ objectFit: "contain" }}
-			/>
+			<Link href="/leaderboard">
+				<Image
+					fill
+					src="/mkttl-logo.png"
+					alt="MK Table Tennis Logo"
+					priority
+					sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+					style={{ objectFit: "contain" }}
+				/>
+			</Link>
 		</div>
 	);
 }
