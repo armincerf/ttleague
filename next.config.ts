4128424a1c3191fc0fce5f3fd0171c6ae1998834
Alex Davis <alx@juxt.pro>
2024-11-10 22:18:02 +0000

    cloudflare pre

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
+if (process.env.NODE_ENV === "development") {
+	setupDevPlatform();
+}
 
 export default {
 	typescript: {
@@ -37,4 +42,4 @@ export default {
 			},
 		];
 	},
-};
+} satisfies NextConfig;
